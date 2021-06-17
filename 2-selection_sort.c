f5ad798f4a53b8431190ea63f7ac002686ada088
B1ueberry121 <2710@holbertonschool.com>
2021-06-17 18:06:49 +0000

    betty fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -3,7 +3,7 @@
 /**
  * selection_sort - sorts an array of integers
  * @array: array of integers to sort
- * @size of the array
+ * @size: size of the array
  */
 
 void selection_sort(int *array, size_t size)
